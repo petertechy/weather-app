2e79d0cbc0dd67dc7b146601bc3a2542fabb9db4
Olanrewaju <ikolabaolanrewaju@gmail.com>
2024-07-25 11:06:49 +0100

    new commit

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-let btn = document.getElementById('add');
-let city = document.getElementById('cityoutput');
-let description = document.getElementById('description');
-let temp = document.getElementById('temp');
-let wind = document.getElementById('wind');
-let apik = "3045dd712ffe6e702e3245525ac7fa38";
+const apiKey = "3045dd712ffe6e702e3245525ac7fa38";
 
-function convertion(val) {
-  return (val - 273).toFixed(2);
+function convertKelvinToCelsius(kelvin) {
+  return (kelvin - 273).toFixed(2);
 }
 
-btn.addEventListener('click', function () {
-  let inputval = document.getElementById('cityinput').value;
-  
-  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputval + '&appid=' + apik)
-    .then(res => res.json())
-    .then(data => {
-      let nameval = data['name'];
-      let descripText = data['weather'][0]['description'];
-      let tempature = data['main']['temp'];
-      let wndspd = data['wind']['speed'];
-      city.innerHTML = `Weather of <span>${nameval}</span>`;
-      temp.innerHTML = `Temperature: <span>${convertion(tempature)} C</span>`;
-      description.innerHTML = `Sky Conditions: <span>${descripText}</span>`;
-      wind.innerHTML = `Wind Speed: <span>${wndspd} km/h</span>`;
-    })
-    .catch(err => alert('You entered the wrong city name'))
+document.getElementById('add').addEventListener('click', async function () {
+  const inputVal = document.getElementById('cityinput').value;
+  const weatherInfo = document.getElementById('weatherInfo');
+
+  try {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}`);
+    const data = await response.json();
+
+    const cityName = data.name;
+    const weatherDescription = data.weather[0].description;
+    const temperature = convertKelvinToCelsius(data.main.temp);
+    const windSpeed = data.wind.speed;
+
+    weatherInfo.innerHTML = `
+      <div class="text-center text-danger fs-2">
+        <p class="animate">Weather of <span class="fw-bold">${cityName}</span></p>
+        <p class="animate">Temperature: <span class="fw-bold">${temperature} °C</span></p>
+        <p class="animate">Sky Conditions: <span class="fw-bold">${weatherDescription}</span></p>
+        <p class="animate">Wind Speed: <span class="fw-bold">${windSpeed} km/h</span></p>
+      </div>
+    `;
+  } catch (err) {
+    weatherInfo.innerHTML = '<p class="text-danger text-center fw-bold display-5">Error: You entered the wrong city name</p>';
+  }
 });
